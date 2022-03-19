4ac04bbb88df8aadfef4131aafaa878b791ddde0
Jack <jack.morgan.edwards@gmail.com>
2022-03-19 15:41:51 -0400

    views and database control implemented

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,41 +1,253 @@
 let Category = require("../models/category");
+var Item = require("../models/item");
+var async = require("async");
+const { body, validationResult } = require("express-validator");
 
 // Display list of all Categories.
-exports.category_list = function (req, res) {
-  res.send("NOT IMPLEMENTED: Category list");
+exports.category_list = function (req, res, next) {
+  Category.find()
+    .sort([["name", "ascending"]])
+    .exec(function (err, list_categories) {
+      if (err) {
+        return next(err);
+      }
+      //Successful, so render
+      res.render("category_list", {
+        title: "Pet Types",
+        category_list: list_categories,
+      });
+    });
 };
 
 // Display detail page for a specific Category.
 exports.category_detail = function (req, res) {
-  res.send("NOT IMPLEMENTED: Category detail: " + req.params.id);
+  async.parallel(
+    {
+      category: function (callback) {
+        Category.findById(req.params.id).exec(callback);
+      },
+
+      category_items: function (callback) {
+        Item.find({ category: req.params.id }).exec(callback);
+      },
+    },
+    function (err, results) {
+      if (err) {
+        return next(err);
+      }
+      if (results.category == null) {
+        // No results.
+        var err = new Error("Category not found");
+        err.status = 404;
+        return next(err);
+      }
+      // Successful, so render
+      res.render("category_detail", {
+        title: "Category Detail",
+        category: results.category,
+        category_items: results.category_items,
+      });
+    }
+  );
 };
 
 // Display Category create form on GET.
 exports.category_create_get = function (req, res) {
-  res.send("NOT IMPLEMENTED: Category create GET");
+  res.render("category_form", { title: "Create Category" });
 };
 
 // Handle Category create on POST.
-exports.category_create_post = function (req, res) {
-  res.send("NOT IMPLEMENTED: Category create POST");
-};
+exports.category_create_post = [
+  // Validate and sanitize the name field.
+  body("name", "Category name required").trim().isLength({ min: 1 }).escape(),
+  body("description", "Description required")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+
+  // Process request after validation and sanitization.
+  (req, res, next) => {
+    // Extract the validation errors from a request.
+    const errors = validationResult(req);
+
+    // Create a category object with escaped and trimmed data.
+    var category = new Category({
+      name: req.body.name,
+      description: req.body.description,
+    });
+
+    if (!errors.isEmpty()) {
+      // There are errors. Render the form again with sanitized values/error messages.
+      res.render("category_form", {
+        title: "Create Category",
+        category: category,
+        errors: errors.array(),
+      });
+      return;
+    } else {
+      // Data from form is valid.
+      // Check if Category with same name already exists.
+      Category.findOne({
+        name: req.body.name,
+        description: req.body.description,
+      }).exec(function (err, found_category) {
+        if (err) {
+          return next(err);
+        }
+
+        if (found_category) {
+          // Category exists, redirect to its detail page.
+          res.redirect(found_category.url);
+        } else {
+          category.save(function (err) {
+            if (err) {
+              return next(err);
+            }
+            // Category saved. Redirect to genre detail page.
+            res.redirect(category.url);
+          });
+        }
+      });
+    }
+  },
+];
 
 // Display Category delete form on GET.
-exports.category_delete_get = function (req, res) {
-  res.send("NOT IMPLEMENTED: Category delete GET");
+exports.category_delete_get = function (req, res, next) {
+  async.parallel(
+    {
+      category: function (callback) {
+        Category.findById(req.params.id).exec(callback);
+      },
+      categories_items: function (callback) {
+        Item.find({ category: req.params.id }).exec(callback);
+      },
+    },
+    function (err, results) {
+      if (err) {
+        return next(err);
+      }
+      if (results.category == null) {
+        // No results.
+        res.redirect("/catalog/categories");
+      }
+      // Successful, so render.
+      res.render("category_delete", {
+        title: "Delete Category",
+        category: results.category,
+        categories_items: results.categories_items,
+      });
+    }
+  );
 };
 
 // Handle Category delete on POST.
-exports.category_delete_post = function (req, res) {
-  res.send("NOT IMPLEMENTED: Category delete POST");
+exports.category_delete_post = function (req, res, next) {
+  async.parallel(
+    {
+      category: function (callback) {
+        Category.findById(req.body.categoryid).exec(callback);
+      },
+      categories_items: function (callback) {
+        Item.find({ category: req.body.categoryid }).exec(callback);
+      },
+    },
+    function (err, results) {
+      if (err) {
+        return next(err);
+      }
+      // Success
+      if (results.categories_items.length > 0) {
+        // Author has books. Render in same way as for GET route.
+        res.render("category_delete", {
+          title: "Delete Category",
+          category: results.category,
+          categories_items: results.categories_items,
+        });
+        return;
+      } else {
+        // Author has no books. Delete object and redirect to the list of authors.
+        Category.findByIdAndRemove(
+          req.body.categoryid,
+          function deleteCategory(err) {
+            if (err) {
+              return next(err);
+            }
+            // Success - go to author list
+            res.redirect("/catalog/categories");
+          }
+        );
+      }
+    }
+  );
 };
 
 // Display Category update form on GET.
-exports.category_update_get = function (req, res) {
-  res.send("NOT IMPLEMENTED: Category update GET");
+exports.category_update_get = function (req, res, next) {
+  Category.findById(req.params.id, function (err, category) {
+    if (err) {
+      return next(err);
+    }
+    if (category == null) {
+      // No results.
+      var err = new Error("Genre not found");
+      err.status = 404;
+      return next(err);
+    }
+    // Success.
+    res.render("category_form", {
+      title: "Update Category",
+      category: category,
+    });
+  });
 };
 
 // Handle Category update on POST.
-exports.category_update_post = function (req, res) {
-  res.send("NOT IMPLEMENTED: Category update POST");
-};
+exports.category_update_post = [
+  // Validate and sanitze the name field.
+  body("name", "Category name must not be empty")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("description", "Description must not be empty")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+
+  // Process request after validation and sanitization.
+  (req, res, next) => {
+    // Extract the validation errors from a request .
+    const errors = validationResult(req);
+
+    // Create a genre object with escaped and trimmed data (and the old id!)
+    var category = new Category({
+      name: req.body.name,
+      description: req.body.description,
+      _id: req.params.id,
+    });
+
+    if (!errors.isEmpty()) {
+      // There are errors. Render the form again with sanitized values and error messages.
+      res.render("category_form", {
+        title: "Update Category",
+        category: category,
+        errors: errors.array(),
+      });
+      return;
+    } else {
+      // Data from form is valid. Update the record.
+      Category.findByIdAndUpdate(
+        req.params.id,
+        category,
+        {},
+        function (err, thecategory) {
+          if (err) {
+            return next(err);
+          }
+          // Successful - redirect to genre detail page.
+          res.redirect(thecategory.url);
+        }
+      );
+    }
+  },
+];
--- controllers/itemController.js
+++ controllers/itemController.js
@@ -1,44 +1,353 @@
 let Item = require("../models/item");
+let Category = require("../models/category");
+const { body, validationResult } = require("express-validator");
+
+let async = require("async");
 
 exports.index = function (req, res) {
-  res.send("NOT IMPLEMENTED: Site Home Page");
+  async.parallel(
+    {
+      item_count: function (callback) {
+        Item.countDocuments({}, callback); // Pass an empty object as match condition to find all documents of this collection
+      },
+      category_count: function (callback) {
+        Category.countDocuments({}, callback);
+      },
+    },
+    function (err, results) {
+      res.render("index", {
+        title: "Home",
+        error: err,
+        data: results,
+      });
+    }
+  );
 };
 
 exports.item_list = function (req, res) {
-  res.send("NOT IMPLEMENTED: Item list");
+  Item.find({}, "name price number_in_stock")
+    .sort({ title: 1 })
+    .populate("price")
+    .exec(function (err, list_items) {
+      if (err) {
+        return next(err);
+      }
+      //Successful, so render
+      res.render("item_list", { title: "Pet List", item_list: list_items });
+    });
 };
 
 // Display detail page for a specific Item.
 exports.item_detail = function (req, res) {
-  res.send("NOT IMPLEMENTED: Item detail: " + req.params.id);
+  async.parallel(
+    {
+      item: function (callback) {
+        Item.findById(req.params.id)
+          .populate("name")
+          .populate("category")
+          .exec(callback);
+      },
+    },
+    function (err, results) {
+      if (err) {
+        return next(err);
+      }
+      if (results.item == null) {
+        // No results.
+        var err = new Error("Book not found");
+        err.status = 404;
+        return next(err);
+      }
+      // Successful, so render.
+      res.render("item_detail", {
+        title: results.item.name,
+        item: results.item,
+      });
+    }
+  );
 };
 
 // Display Item create form on GET.
-exports.item_create_get = function (req, res) {
-  res.send("NOT IMPLEMENTED: Item create GET");
+exports.item_create_get = function (req, res, next) {
+  // Get all authors and genres, which we can use for adding to our book.
+  async.parallel(
+    {
+      items: function (callback) {
+        Item.find(callback);
+      },
+      categories: function (callback) {
+        Category.find(callback);
+      },
+    },
+    function (err, results) {
+      if (err) {
+        return next(err);
+      }
+      res.render("item_form", {
+        title: "Create Item",
+        items: results.items,
+        categories: results.categories,
+      });
+    }
+  );
 };
 
-// Handle Item create on POST.
-exports.item_create_post = function (req, res) {
-  res.send("NOT IMPLEMENTED: Item create POST");
-};
+// Handle item create on POST.
+exports.item_create_post = [
+  // Convert the genre to an array.
+  (req, res, next) => {
+    if (!(req.body.category instanceof Array)) {
+      if (typeof req.body.category === "undefined") req.body.category = [];
+      else req.body.category = new Array(req.body.category);
+    }
+    next();
+  },
+
+  // Validate and sanitize fields.
+  body("name", "Name must not be empty.").trim().isLength({ min: 1 }).escape(),
+  body("price", "Summary must not be empty.")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("number_in_stock", "Number in stock must not be empty")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("category.*").escape(),
+
+  // Process request after validation and sanitization.
+  (req, res, next) => {
+    // Extract the validation errors from a request.
+    const errors = validationResult(req);
+
+    // Create a Book object with escaped and trimmed data.
+    var item = new Item({
+      name: req.body.name,
+      price: req.body.price,
+      number_in_stock: req.body.number_in_stock,
+      category: req.body.category,
+    });
+
+    if (!errors.isEmpty()) {
+      // There are errors. Render form again with sanitized values/error messages.
+
+      // Get all authors and genres for form.
+      async.parallel(
+        {
+          categories: function (callback) {
+            Category.find(callback);
+          },
+        },
+        function (err, results) {
+          if (err) {
+            return next(err);
+          }
+
+          // Mark our selected genres as checked.
+          for (let i = 0; i < results.categories.length; i++) {
+            if (item.category.indexOf(results.categories[i]._id) > -1) {
+              results.categories[i].checked = "true";
+            }
+          }
+          res.render("item_form", {
+            title: "Create Item",
+            categories: results.categories,
+            item: item,
+            errors: errors.array(),
+          });
+        }
+      );
+      return;
+    } else {
+      // Data from form is valid. Save book.
+      item.save(function (err) {
+        if (err) {
+          return next(err);
+        }
+        //successful - redirect to new book record.
+        res.redirect(item.url);
+      });
+    }
+  },
+];
 
 // Display Item delete form on GET.
-exports.item_delete_get = function (req, res) {
-  res.send("NOT IMPLEMENTED: Item delete GET");
+exports.item_delete_get = function (req, res, next) {
+  async.parallel(
+    {
+      item: function (callback) {
+        Item.findById(req.params.id).exec(callback);
+      },
+    },
+    function (err, results) {
+      if (err) {
+        return next(err);
+      }
+      if (results.item == null) {
+        // No results.
+        res.redirect("/catalog/items");
+      }
+      // Successful, so render.
+      res.render("item_delete", {
+        title: "Delete Item",
+        item: results.item,
+      });
+    }
+  );
 };
 
 // Handle Item delete on POST.
-exports.item_delete_post = function (req, res) {
-  res.send("NOT IMPLEMENTED: Item delete POST");
+exports.item_delete_post = function (req, res, next) {
+  async.parallel(
+    {
+      item: function (callback) {
+        Item.findById(req.body.itemid).exec(callback);
+      },
+    },
+    function (err, results) {
+      if (err) {
+        return next(err);
+      } else {
+        Item.findByIdAndRemove(req.body.itemid, function deleteItemy(err) {
+          if (err) {
+            return next(err);
+          }
+          // Success - go to author list
+          res.redirect("/catalog/items");
+        });
+      }
+    }
+  );
 };
 
-// Display Item update form on GET.
-exports.item_update_get = function (req, res) {
-  res.send("NOT IMPLEMENTED: Item update GET");
+// Display book update form on GET.
+exports.item_update_get = function (req, res, next) {
+  // Get book, authors and genres for form.
+  async.parallel(
+    {
+      item: function (callback) {
+        Item.findById(req.params.id).populate("category").exec(callback);
+      },
+      categories: function (callback) {
+        Category.find(callback);
+      },
+    },
+    function (err, results) {
+      if (err) {
+        return next(err);
+      }
+      if (results.item == null) {
+        // No results.
+        var err = new Error("Book not found");
+        err.status = 404;
+        return next(err);
+      }
+      // Success.
+      // Mark our selected genres as checked.
+      for (
+        var all_g_iter = 0;
+        all_g_iter < results.categories.length;
+        all_g_iter++
+      ) {
+        for (
+          var book_g_iter = 0;
+          book_g_iter < results.item.category.length;
+          book_g_iter++
+        ) {
+          if (
+            results.categories[all_g_iter]._id.toString() ===
+            results.item.category[book_g_iter]._id.toString()
+          ) {
+            results.categories[all_g_iter].checked = "true";
+          }
+        }
+      }
+      res.render("item_form", {
+        title: "Update Item",
+        categories: results.categories,
+        item: results.item,
+      });
+    }
+  );
 };
 
-// Handle Item update on POST.
-exports.item_update_post = function (req, res) {
-  res.send("NOT IMPLEMENTED: Item update POST");
-};
+// Handle book update on POST.
+exports.item_update_post = [
+  // Convert the genre to an array
+  (req, res, next) => {
+    if (!(req.body.category instanceof Array)) {
+      if (typeof req.body.category === "undefined") req.body.category = [];
+      else req.body.category = new Array(req.body.category);
+    }
+    next();
+  },
+
+  // Validate and sanitize fields.
+  body("name", "Name must not be empty.").trim().isLength({ min: 1 }).escape(),
+  body("price", "Price must not be empty.")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("number_in_stock", "Stock must not be empty")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("category.*").escape(),
+
+  // Process request after validation and sanitization.
+  (req, res, next) => {
+    // Extract the validation errors from a request.
+    const errors = validationResult(req);
+
+    // Create a Book object with escaped/trimmed data and old id.
+    var item = new Item({
+      name: req.body.name,
+      price: req.body.price,
+      number_in_stock: req.body.number_in_stock,
+      category:
+        typeof req.body.category === "undefined" ? [] : req.body.category,
+      _id: req.params.id, //This is required, or a new ID will be assigned!
+    });
+
+    if (!errors.isEmpty()) {
+      // There are errors. Render form again with sanitized values/error messages.
+
+      // Get all authors and genres for form.
+      async.parallel(
+        {
+          categories: function (callback) {
+            Category.find(callback);
+          },
+        },
+        function (err, results) {
+          if (err) {
+            return next(err);
+          }
+
+          // Mark our selected genres as checked.
+          for (let i = 0; i < results.categories.length; i++) {
+            if (item.category.indexOf(results.categories[i]._id) > -1) {
+              results.categories[i].checked = "true";
+            }
+          }
+          res.render("item_form", {
+            title: "Update Item",
+            categories: results.categories,
+            item: item,
+            errors: errors.array(),
+          });
+        }
+      );
+      return;
+    } else {
+      // Data from form is valid. Update the record.
+      Item.findByIdAndUpdate(req.params.id, item, {}, function (err, theitem) {
+        if (err) {
+          return next(err);
+        }
+        // Successful - redirect to book detail page.
+        res.redirect(theitem.url);
+      });
+    }
+  },
+];
